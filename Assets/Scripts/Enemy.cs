8d6d4b9928c936b2e52865b99f959ca674841d2a
Vasilis Gkagkakis <vasilis7gagakis@gmail.com>
2025-06-20 16:19:08 +0300

    Created pooling system for enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float MaxHealth;
+    public float Health;
+    public float Speed;
+    public int ID;
+
+    public void Init()
+    {
+        Health = MaxHealth;
+    }
+    
+}
---
+++ Assets/Scripts/EntitySummoner.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EntitySummoner : MonoBehaviour
+{
+    public static List<Enemy> EnemiesInGame;
+    public static Dictionary<int, GameObject> EnemyPrefabs;
+    public static Dictionary<int, Queue<Enemy>> EnemyObjectPools;
+
+    private static bool IsInitialized;
+    public static void Init()
+    {
+        if (!IsInitialized)
+        {
+            EnemyPrefabs = new Dictionary<int, GameObject>();
+            EnemyObjectPools = new Dictionary<int, Queue<Enemy>>();
+            EnemiesInGame = new List<Enemy>();
+
+            EnemySummonData[] Enemies = Resources.LoadAll<EnemySummonData>("Enemies");
+            Debug.Log("Found " + Enemies.Length + " enemy prefabs in Resources/Enemies");
+
+            foreach (EnemySummonData enemyData in Enemies)
+            {
+                EnemyPrefabs.Add(enemyData.EnemyID, enemyData.EnemyPrefab);
+                EnemyObjectPools.Add(enemyData.EnemyID, new Queue<Enemy>());
+            }
+
+            IsInitialized = true;
+        }
+        else
+        {
+            Debug.LogWarning("EntitySummoner is already initialized.");
+        }
+    }
+
+    public static Enemy SummonEnemy(int EnemyID)
+    {
+        Enemy SummonedEnemy = null;
+
+        if (EnemyPrefabs.ContainsKey(EnemyID))
+        {
+            Queue<Enemy> ReferencedQueue = EnemyObjectPools[EnemyID];
+
+            if (ReferencedQueue.Count > 0)
+            {
+                //Dequeue existing enemy from the pool
+                SummonedEnemy = ReferencedQueue.Dequeue();
+                SummonedEnemy.Init();
+
+                SummonedEnemy.gameObject.SetActive(true);
+                Debug.Log("Reusing enemy from pool with ID: " + EnemyID);
+            }
+            else
+            {
+                GameObject enemyPrefab = Instantiate(EnemyPrefabs[EnemyID], Vector3.zero, Quaternion.identity);
+                SummonedEnemy = enemyPrefab.GetComponent<Enemy>();
+                SummonedEnemy.Init();
+                Debug.Log("Instantiated new enemy with ID: " + EnemyID);
+            }
+
+            SummonedEnemy.ID = EnemyID;
+            SummonedEnemy.Init();
+            EnemiesInGame.Add(SummonedEnemy);
+        }
+        else
+        {
+            Debug.Log("Enemy ID " + EnemyID + " not found in EnemyPrefabs.");
+            return null;
+        }
+
+        EnemiesInGame.Add(SummonedEnemy);
+        SummonedEnemy.ID = EnemyID;
+        return SummonedEnemy;
+    }
+
+    public static void RemoveEnemy(Enemy EnemyToRemove)
+    {
+        EnemyObjectPools[EnemyToRemove.ID].Enqueue(EnemyToRemove);
+        EnemyToRemove.gameObject.SetActive(false);
+        EnemiesInGame.Remove(EnemyToRemove);
+    }
+
+}
